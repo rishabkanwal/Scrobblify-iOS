5c27767a0761c9bae94d0ab33458efd17fb8dfd0
Rishab Kanwal <rishabkanwal@gmail.com>
2017-06-11 03:14:54 -0600

    Initial Commit

---
+++ Scrobblify/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  Scrobblify
+//
+//  Created by Rishab Kanwal on 6/11/17.
+//  Copyright © 2017 Rishab Kanwal. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
