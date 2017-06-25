eac8ae7518dd7678d060490cd89c1c14fa4ba83c
Rishab Kanwal <rishabkanwal@gmail.com>
2017-06-25 09:01:45 -0600

    Added empty settings

---
+++ Scrobblify/ViewControllers/SettingsViewController.swift
@@ -0,0 +1,18 @@
+//
+//  SettingsViewController.swift
+//  Scrobblify
+//
+//  Created by Rishab Kanwal on 6/25/17.
+//  Copyright © 2017 Rishab Kanwal. All rights reserved.
+//
+
+import UIKit
+
+
+class SettingsViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+}
